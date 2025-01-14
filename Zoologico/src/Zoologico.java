4e1860ff1c5cb14b93af8267d99d08d843bf7491
Bruno Pereira <brunopereira@acad.ifma.edu.br>
2025-01-14 18:47:26 -0300

    zoo

--- Zoologico/src/Zoologico.java
+++ Zoologico/src/Zoologico.java
@@ -1,27 +1,3 @@
-import animal.Animal;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-
 public class Zoologico {
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int op;
-        while (true){
-            System.out.println("---------- ZOOLOGICO ----------\n");
-            System.out.println("1 - Cadastrar animal");
-            System.out.println("2 - Cadastrar funcionario");
-            System.out.println("0 - Sair");
-            System.out.print(">> ");
-            op = scan.nextInt();
-            if (op == 0){
-                break;
-            }
-            if (op == 1){
-                System.out.println("---------- CADASTRO DE ANIMAL ----------");
-            }
-        }
-
-    }
+    
 }
--- Zoologico/src/funcionario/Funcionario.java
+++ Zoologico/src/funcionario/Funcionario.java
@@ -7,6 +7,31 @@ public class Funcionario {
     private String nome;
     private String especialidade;
 
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEspecialidade() {
+        return especialidade;
+    }
+
+    public void setEspecialidade(String especialidade) {
+        this.especialidade = especialidade;
+    }
+
     void cuidarAnimal(Animal animal){
         System.out.println("Cuidando do animal: " + animal);
     }
